79492cc7d6fbaf48e8d284053e8a51bf3968c12f
Chwaksen <agustinmandarini47@gmail.com>
2021-12-14 19:10:32 -0300

    First Commit

---
+++ auctions/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class NewListing(forms.Form):
+
+	title = forms.CharField(max_length=80)
+	descr = forms.CharField(widget=forms.Textarea)
+	starting_bid = forms.DecimalField(max_value=1000000, decimal_places=2)
---
+++ auctions/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+	title = models.CharField(max_length=80)
+	descr = models.TextField()
+	starting_bid = models.DecimalField(max_digits=7, decimal_places=2)
+	creation_date = models.DateField(auto_now_add=True)
+	close_date = models.DateField(auto_now=True)
+	status = models.BooleanField(default=True)
+	seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="u_listings")
+
+	def __str__(self):
+		return f"{self.title} {self.descr} {self.creation_date} {self.starting_bid} {self.close_date} {self.l_comments}"
+
+class Bid(models.Model):
+
+	value = models.DecimalField(max_digits=7, decimal_places=2)
+	bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="u_bids")
+	listing_bid = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="l_bids")
+
+	def __str__(self):
+		return f"{self.value} {self.bidder} {self.listing_bid}"
+
+class Comments(models.Model):
+
+	content = models.CharField(max_length=1000)
+	creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="u_comments")
+	listing_comment = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="l_comments")
+
+	def __str__(self):
+		return f"{self.content} {self.creator} {self.listing_comment}"
+
